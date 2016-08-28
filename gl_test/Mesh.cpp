4cb9c1c0760082fc062af04edbd820bf3fdd91aa
Brett Bennett <bbennett905@gmail.com>
2016-08-28 10:45:51 -0400

    Add to mesh + model

--- gl_test/Mesh.cpp
+++ gl_test/Mesh.cpp
@@ -1,15 +1,54 @@
 #include "Mesh.h"
 
-Mesh::Mesh(std::vector<VertexData> vert, std::vector<GLuint> ind, std::vector<TextureData> texts) :
-	BaseDrawable(vert, ind), TextData(texts)
+#include <gtc\matrix_transform.hpp>
+
+Mesh::Mesh(std::vector<VertexData> vert, std::vector<GLuint> ind, std::vector<Texture> texts) :
+	BaseDrawable(vert, ind), Textures(texts)
 {
 
+}
+
+glm::mat4 Mesh::GetModelMatrix()
+{
+	glm::mat4 model;
+	model = glm::translate(model, Position);
+
+	glm::vec3 ang = GetAngles();
+	model = glm::rotate(model, glm::radians(ang.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(ang.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(ang.z), glm::vec3(0.0f, 0.0f, 1.0f));
+
+	return model;
 }
 
 
 Model::Model(const char * path)
 {
 	loadModel(path);
+}
+
+std::vector<Mesh> * Model::GetMeshes()
+{
+	return &_meshes;
+}
+
+void Model::SetPosition(glm::vec3 pos)
+{
+	Position = pos;
+	for (uint32_t i = 0; i < _meshes.size(); i++)
+		_meshes[i].Position = pos; //i THINK this should work
+}
+
+glm::vec3 Model::GetPosition()
+{
+	return Position;
+}
+
+void Model::SetAngles(glm::vec3 ang)
+{
+	BaseObject::SetAngles(ang);
+	for (uint32_t i = 0; i < _meshes.size(); i++)
+		_meshes[i].SetAngles(ang); //i THINK this should work
 }
 
 void Model::loadModel(std::string path)
@@ -30,7 +69,7 @@ void Model::processNode(aiNode * node, const aiScene * scene)
 	for (uint32_t i = 0; i < node->mNumMeshes; i++)
 	{
 		aiMesh * mesh = scene->mMeshes[node->mMeshes[i]];
-		Meshes.push_back(processMesh(mesh, scene));
+		_meshes.push_back(processMesh(mesh, scene));
 	}
 	for (uint32_t i = 0; i < node->mNumChildren; i++)
 		processNode(node->mChildren[i], scene);
@@ -38,5 +77,79 @@ void Model::processNode(aiNode * node, const aiScene * scene)
 
 Mesh Model::processMesh(aiMesh * mesh, const aiScene * scene)
 {
-	return Mesh();
+	std::vector<VertexData> vertices;
+	std::vector<GLuint> indices;
+	std::vector<Texture> textures;
+	
+	for (uint32_t i = 0; i < mesh->mNumVertices; i++)
+	{
+		VertexData vertex;
+		glm::vec3 vec;
+		vec.x = mesh->mVertices[i].x;
+		vec.y = mesh->mVertices[i].y;
+		vec.z = mesh->mVertices[i].z;
+		vertex.Position = vec;
+
+		vec.x = mesh->mNormals[i].x;
+		vec.y = mesh->mNormals[i].y;
+		vec.z = mesh->mNormals[i].z;
+		vertex.Normal = vec;
+
+		if (mesh->mTextureCoords[0])
+		{
+			glm::vec2 vector;
+			vector.x = mesh->mTextureCoords[0][i].x;
+			vector.y = mesh->mTextureCoords[0][i].y;
+			vertex.TexCoords = vector;
+		}
+		else vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+		vertices.push_back(vertex);
+	}
+
+	for (uint32_t i = 0; i < mesh->mNumFaces; i++)
+	{
+		aiFace face = mesh->mFaces[i];
+		for (uint32_t j = 0; j < face.mNumIndices; j++)
+			indices.push_back(face.mIndices[j]);
+	}
+
+	if (mesh->mMaterialIndex >= 0)
+	{
+		aiMaterial * material = scene->mMaterials[mesh->mMaterialIndex];
+
+		std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+		textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+	
+		std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+		textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+	}
+
+	return Mesh(vertices, indices, textures);
+}
+
+std::vector<Texture> Model::loadMaterialTextures(aiMaterial * mat, aiTextureType type, std::string typename)
+{
+	std::vector<Texture> textures;
+	for (uint32_t i = 0; i < mat->GetTextureCount(type); i++)
+	{
+		aiString str;
+		mat->GetTexture(type, i, &str);
+		bool skip = false;
+		for (uint32_t j = 0; j < _textures_loaded.size(); j++)
+		{
+			if (_textures_loaded[j].Path == str)
+			{
+				textures.push_back(_textures_loaded[j]);
+				skip = true;
+				break;
+			}
+		}
+		if (!skip)
+		{
+			Texture texture(str.C_Str());
+			textures.push_back(texture);
+			_textures_loaded.push_back(texture);
+		}
+	}
+	return textures;
 }
--- gl_test/Mesh.h
+++ gl_test/Mesh.h
@@ -6,32 +6,35 @@
 #include <scene.h>
 #include <postprocess.h>
 
-struct TextureData
-{
-	GLuint id;
-	std::string type;
-	aiString path;
-};
-
 //TODO separate these two!
 class Mesh :
-	public BaseDrawable
+	public BaseDrawable, public BaseObject
 {
 public:
-	Mesh(std::vector<VertexData> vert, std::vector<GLuint> ind, std::vector<TextureData> texts);
+	Mesh(std::vector<VertexData> vert, std::vector<GLuint> ind, std::vector<Texture> texts);
 	glm::mat4 GetModelMatrix();
-	std::vector<TextureData> TextData;
+	std::vector<Texture> Textures;
 };
 
 class Model :
 	public BaseObject
 {
 public:
-	std::vector<Mesh> Meshes;
 	Model(const char * path);
+	std::vector<Mesh> * GetMeshes();
+
+	void SetPosition(glm::vec3 pos);
+	glm::vec3 GetPosition();
+	void SetAngles(glm::vec3 ang);
 private:
 	void loadModel(std::string path);
 	void processNode(aiNode * node, const aiScene * scene);
 	Mesh processMesh(aiMesh * mesh, const aiScene * scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial * mat, aiTextureType type, std::string typename);
+	
+	std::vector<Mesh> _meshes;
 	std::string _directory;
+	std::vector<Texture> _textures_loaded;
+
+	glm::vec3 Position;
 };
