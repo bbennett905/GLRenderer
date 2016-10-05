3147947ab0101880920042dca5a8217e3628165a
Brett Bennett <bbennett905@gmail.com>
2016-10-05 18:47:05 -0400

    Add to Scene class

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,6 +1,7 @@
 #include "Scene.h"
 
-
+#include "BaseDrawable.h"
+#include "BaseObject.h"
 
 Scene::Scene()
 {
@@ -9,4 +10,29 @@ Scene::Scene()
 
 Scene::~Scene()
 {
+}
+
+void Scene::AddObjectToScene(BaseObject * obj)
+{
+	//TODO test this! its very likely that this doesnt quite work this way
+	_object_list.push_back(obj);
+
+	BaseDrawable * drawable = dynamic_cast<BaseDrawable *>(obj);
+	if (drawable)
+	{
+		_draw_list.push_back(drawable);
+	}
+}
+
+void Scene::Update(double delta_time)
+{
+	//TODO implement BaseObject::Update
+	for (auto obj : _object_list)
+	{
+		//obj->Update(delta_time);
+	}
+	for (auto draw : _draw_list)
+	{
+		draw->Draw(_scene_renderer);
+	}
 }
--- src/Scene.h
+++ src/Scene.h
@@ -1,18 +1,29 @@
 #pragma once
 
-#include "SceneRenderer.h"
+#include <vector>
 //this will have a SceneRenderer, containing light info and such?
 //also this will have a list of all objects in scene, and call BaseDrawable::Draw(SceneRenderer)
 //each frame. Or that could be within the scenerenderer, which passes relevant information to 
 //BaseDrawable::Draw(lights, etc)
+
+class SceneRenderer;
+class BaseDrawable;
+class BaseObject;
+
 class Scene
 {
 public:
 	Scene();
 	~Scene();
 
+	void AddObjectToScene(BaseObject * obj);
+
+	//TODO have draw inside update? or called externally?
+	void Update(double delta_time);
+
 private:
 	SceneRenderer * _scene_renderer;
-
+	std::vector<BaseDrawable *> _draw_list;
+	std::vector<BaseObject *> _object_list;
 };
 
