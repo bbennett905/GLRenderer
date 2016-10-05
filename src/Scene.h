03a60937054b5f663c287d00b80e4f942403aa72
Brett Bennett <bbennett905@gmail.com>
2016-10-05 14:12:35 -0400

    Begin layout of Scene and SceneRenderer

--- src/Scene.h
+++ src/Scene.h
@@ -1,6 +1,6 @@
 #pragma once
 
-
+#include "SceneRenderer.h"
 //this will have a SceneRenderer, containing light info and such?
 //also this will have a list of all objects in scene, and call BaseDrawable::Draw(SceneRenderer)
 //each frame. Or that could be within the scenerenderer, which passes relevant information to 
@@ -10,5 +10,9 @@ class Scene
 public:
 	Scene();
 	~Scene();
+
+private:
+	SceneRenderer * _scene_renderer;
+
 };
 
--- src/SceneRenderer.h
+++ src/SceneRenderer.h
@@ -1,11 +1,27 @@
 #pragma once
 
+#include <vector>
+#include "Lights.h"
+#include "Camera.h"
+
 //see Scene.h for most info,
 //but this is basically what is currently the Renderer class with a little moved to BaseDrawable
+//DOES NOT NEED A WINDOW ARG
 class SceneRenderer
 {
 public:
 	SceneRenderer();
 	~SceneRenderer();
+
+	//TODO
+	//Add light method
+	//Set Light uniforms method
+
+private:
+	std::vector<LightPoint *>	_point_light_list;
+	std::vector<LightSpot *>	_spot_light_list;
+	LightDirectional *			_directional_light;
+
+	Camera * _camera;
 };
 
