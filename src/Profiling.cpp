50b884615b1e03204880ab32e42ff6b1ecde75be
Brett Bennett <bbennett905@gmail.com>
2016-11-02 18:22:11 -0400

    Update Profiling to support parented ProfileSamples

--- src/Profiling.cpp
+++ src/Profiling.cpp
@@ -8,22 +8,54 @@
 
 namespace Profiling
 {
+	std::vector<ProfileData *> ProfileSample::_open_samples;
+
 	ProfileSample::ProfileSample(std::string name) :
-		_name(name)
+		_start_time(Timing::GetTime())
 	{
-		_start_time = Timing::GetTime();
+		//oh god
+		Self = new ProfileData();
+		Self->name = name;
+		Self->num_parents = _open_samples.size();
+
+		//Check if this sample has a parent
+		if (_open_samples.size() >= 1)
+		{
+			//Set the parent to the last sample in the vector
+			Self->parent = _open_samples[_open_samples.size() - 1];
+		}
+		else
+		{
+			Self->parent = nullptr;
+		}
+
+		_open_samples.push_back(Self);
 	}
 
 	ProfileSample::~ProfileSample()
 	{
+		Self->time = Timing::GetSecondsSince(_start_time);
+
 		//so uh threading this made it way slower oops
-		AddProfileData(Timing::GetSecondsSince(_start_time), _name);
+		AddProfileData(Self);
+
+		//Find the current sample in the vector and remove it
+		for (uint32_t i = 0; i < _open_samples.size(); i++)
+		{
+			if (_open_samples[i] == Self)
+			{
+				_open_samples.erase(_open_samples.begin() + i);
+				break;
+			}
+		}
 	}
+
 
 	namespace
 	{
-		std::vector<std::tuple<double, std::string>> _entries;
+		std::vector<ProfileData *> _entries;
 		uint64_t _prof_start_time;
+		double _prof_full_seconds;
 	}
 
 	void ProfInit()
@@ -31,45 +63,65 @@ namespace Profiling
 		_prof_start_time = Timing::GetTime();
 	}
 
-	void AddProfileData(double seconds, std::string name)
+	void AddProfileData(ProfileData * data)
 	{
 		//Realistically we'd want this to be fast, and this might not be so fast - 
 		//but, its going to be about the same speed for each sample, so it'll be fine
 		bool already_exists = false;
 		for (uint32_t i = 0; i < _entries.size(); i++)
 		{
-			if (std::get<1>(_entries[i]) == name)
+			if (_entries[i]->name == data->name)
 			{
 				already_exists = true;
-				std::get<0>(_entries[i]) += seconds;
+				_entries[i]->time += data->time;
 				break;
 			}
 		}
 		if (!already_exists)
 		{
-			_entries.push_back(std::tuple<double, std::string>(seconds, name));
+			_entries.push_back(data);
+		}
+	}
+
+	//Recursive function for printing the entries
+	void printEntries(int level = 0, ProfileData * parent = nullptr)
+	{
+		for (auto entry : _entries)
+		{
+			if (entry->num_parents == level && (parent == nullptr || parent == entry->parent))
+			{
+				std::string buff;
+				for (uint32_t i = 0; i < entry->num_parents; i++) buff += '-';
+				buff += entry->name.c_str();
+				if (parent != nullptr)
+				{
+					Logging::LogMessage(LogLevel_Debug, " %06.5f | %06.3f | %s",
+						entry->time, 100.0 * (entry->time / parent->time),
+						buff.c_str());
+				}
+				else
+				{
+					Logging::LogMessage(LogLevel_Debug, " %06.5f | %06.3f | %s",
+						entry->time, 100.0 * (entry->time / _prof_full_seconds),
+						buff.c_str());
+				}
+				printEntries(level + 1, entry);
+			}
 		}
 	}
 
 	void LogProfileData()
 	{
-		double prof_end_seconds = Timing::GetSecondsSince(_prof_start_time);
+		_prof_full_seconds = Timing::GetSecondsSince(_prof_start_time);
 		Logging::LogMessage(LogLevel_Debug, "-------------------------------");
 		Logging::LogMessage(LogLevel_Debug, " Profiling Output:");
-		Logging::LogMessage(LogLevel_Debug, " Total Execution Time: %f", prof_end_seconds);
+		Logging::LogMessage(LogLevel_Debug, " Total Execution Time: %f", _prof_full_seconds);
 		Logging::LogMessage(LogLevel_Debug, " Entries:");
-		double sample_total = 0.0;
-		for (auto entry : _entries)
-		{
-			sample_total += std::get<0>(entry);
-		}
+
 		Logging::LogMessage(LogLevel_Debug, "   sec   |   %%    | name");
-		for (auto entry : _entries)
-		{
-			Logging::LogMessage(LogLevel_Debug, " %06.5f | %06.3f | %s", 
-				std::get<0>(entry), 100.0 * (std::get<0>(entry) / sample_total), 
-				std::get<1>(entry).c_str());
-		}
+
+		printEntries();
+
 		Logging::LogMessage(LogLevel_Debug, "-------------------------------");
 	}
 }
--- src/Profiling.h
+++ src/Profiling.h
@@ -1,9 +1,25 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace Profiling
 {
+	/*
+	 * Holds data collected from a ProfileSample
+	 */
+	struct ProfileData
+	{
+		//Time, in seconds
+		double time;
+		//Name of this profile
+		std::string name;
+		//Pointer to the parent of this sample, or nullptr if there is none
+		ProfileData * parent;
+		//Number of parents of this sample
+		uint32_t num_parents;
+	};
+
 	/*
 	 * A timed sample - timer starts on creation, ends and submits on destruction
 	 */
@@ -12,9 +28,10 @@ namespace Profiling
 	public:
 		ProfileSample(std::string name);
 		~ProfileSample();
+		ProfileData * Self;
 
 	private:
-		std::string _name;
+		static std::vector<ProfileData *> _open_samples;
 		uint64_t _start_time;
 	};
 
@@ -22,7 +39,7 @@ namespace Profiling
 	void ProfInit();
 
 	//Adds an entry of data to the profiler
-	void AddProfileData(double time, std::string name);
+	void AddProfileData(ProfileData * data);
 
 	//Processes and prints all profile data as LogLevel_Debug messages
 	void LogProfileData();
